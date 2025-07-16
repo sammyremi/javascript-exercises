c5264d87600488f903b690f0931d72cfe1dbc25b
sammyremi <adebayorsamuelas@gmail.com>
2025-07-16 15:39:32 +0100

    repeat string function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){return "ERROR"}
+    if (string === ""){return ""}
+    let str = ""
+    for (x = 0; x < num; x++){
+        str = str + string
+    }
+    return str
 };
 
 // Do not edit below this line
