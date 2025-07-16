6a50eaa226f1f3708b78a9af21d132068bd41fe9
sammyremi <adebayorsamuelas@gmail.com>
2025-07-16 16:07:32 +0100

    reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let string = ""
+    for ( x = str.length - 1; x >= 0; x--){
+        string += str[x]
+    }
+    return string
 };
 
 // Do not edit below this line
