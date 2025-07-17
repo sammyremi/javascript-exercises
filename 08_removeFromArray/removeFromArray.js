1d3369f5f010e2dbd57e725f267bfd4f26398eb8
sammyremi <adebayorsamuelas@gmail.com>
2025-07-17 06:59:55 +0100

    feat: remove element from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    let tempArr = [];
+    for (x = 0; x < arr.length; x++){
+        if (theArgs.includes(arr[x])){continue}else{tempArr.push(arr[x])}
+    }
+    return tempArr
 };
 
 // Do not edit below this line
