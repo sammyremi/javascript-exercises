34cb85b3ee3fc1d30f2bac08ac6b240a20c1233e
sammyremi <adebayorsamuelas@gmail.com>
2025-07-20 00:28:34 +0100

    use ascii table to get palindrom string

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,25 +1,28 @@
-const palindromes = function (string) {
-    // let temp = ""
-    // let count = 0
+const palindromes = function (strings) {
+    let string = ""
+    let temp = ""
+    let count = 0
 
-    // for(x = string.length - 1; x >= 0; x--){
-    //     temp += string[x]
-    // }
-
-    // for (x = 0; x < string.length; x++){
-    //     if (string[x] === temp[x]){
-    //         count += 1
-    //     }
-    // }
-
-    // if (count == string.length){
-    //     return true
-    // }else{return false}
-    console.log()
-
-    for (x = 65; x <= 97; x++){
-        console.log(x)
+    for (x = 0; x < strings.length; x++){
+        if (strings[x].charCodeAt() >= 65 && strings[x].charCodeAt() <= 90 || strings[x].charCodeAt() >= 97 && strings[x].charCodeAt() <= 122 || strings[x].charCodeAt() >= 47 &&  strings[x].charCodeAt() <= 57){
+            string += strings[x].toLowerCase()
+        }
     }
+
+    for(x = string.length - 1; x >= 0; x--){
+        temp += string[x]
+    }
+
+    for (x = 0; x < string.length; x++){
+        if (string[x] === temp[x]){
+            count += 1
+        }
+    }
+
+    if (count == string.length){
+        return true
+    }else{return false}
+
 };
 
 palindromes("racecar")
