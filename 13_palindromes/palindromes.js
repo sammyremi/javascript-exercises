56bcabe6863f07dbf575c17f94fb04f862a90e79
sammyremi <adebayorsamuelas@gmail.com>
2025-07-19 20:42:52 +0100

    leap year, calculator, palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // let temp = ""
+    // let count = 0
 
+    // for(x = string.length - 1; x >= 0; x--){
+    //     temp += string[x]
+    // }
+
+    // for (x = 0; x < string.length; x++){
+    //     if (string[x] === temp[x]){
+    //         count += 1
+    //     }
+    // }
+
+    // if (count == string.length){
+    //     return true
+    // }else{return false}
+    console.log()
+
+    for (x = 65; x <= 97; x++){
+        console.log(x)
+    }
 };
+
+palindromes("racecar")
 
 // Do not edit below this line
 module.exports = palindromes;
